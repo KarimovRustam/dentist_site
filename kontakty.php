848599e35963d8a443d3e739487a4150e92d0b09
Rustam <karimovcode@gmail.com>
2019-03-21 23:12:33 +0300

    add contact page

---
+++ kontakty.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+	<title>Контакты стоматология ФлагманСтом Казань</title>
+	<meta property="og:title" content="Протезирование зубов коронками. Имплантация зубов. Ортодонтические услуги. Отбеливание зубов. Безболезненное удаление зубов. Съемное протезирование. Лечение корневых каналов. Лечение кариеса. Лечение десен. Установка виниров. Детские стоматологические услуги."/>
+	<meta property="og:description" content="Стоматологическая клиника ФлагманСтом Заботимся о вашей улыбке с 2017 года"/>
+	<meta property="og:image" content="https://flagmanstom.ru/forshare.png">
+	<meta name="description" content="Перечень услуг стоматологии ФлагманСтом Казань Протезирование зубов коронками. Имплантация зубов. Ортодонтические услуги. Отбеливание зубов. Безболезненное удаление зубов. Съемное протезирование. Лечение корневых каналов. Лечение кариеса. Лечение десен. Установка виниров. Детские стоматологические услуги.">
+	<meta name="keywords" content="стоматология Казань, стоматологии Казани, дмс стоматология, полис дмс стоматология, согаз дмс стоматология, услуги стоматологии, стоматология платные услуги, детская стоматология казань платные услуги, услуги стоматолога казань, перечень услуг стоматологии, стоматология казань, детская стоматология казань, стоматология казань телефон">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	<link href="css/style.css?v=1123q1w121" rel="stylesheet">
+</head>
+	<body>
+		<?php
+			include("blocks/header.php");
+		?> 
+			<main>
+					<h2>Контакты</h2>
+					<div class="сontact-information">
+						<div class="сontact-information__item our-address">
+							<div class="сontact-information__item--title">
+								Наш адрес
+							</div>
+								<div class="сontact-information__item--text">
+									<p>город Казань</p>
+									<p>ул. Проспект Победы 46А</p>
+								</div>
+						</div>
+						<div class="сontact-information__item our-phones">
+							<div class="сontact-information__item--title">
+								Телефоны
+							</div>
+								<div class="сontact-information__item--text">
+									<p>+7 (843) 251-25-88</p>
+									<p>+7 (904) 712-25-88</p>
+								</div>
+						</div>
+						<div class="сontact-information__item our-workschedule">
+							<div class="сontact-information__item--title">
+								График работы
+							</div>
+								<div class="сontact-information__item--text">
+									<p>Будние дни с 8:00 до 20:00</p>
+									<p>Выходные с 9:00 до 17:00</p>
+									<i>*Мы работаем без перерыва</i>
+								</div>
+						</div>
+						<div class="сontact-information__item additional-information">
+							<div class="сontact-information__item--title">
+								Дополнительная информация
+							</div>
+								<div class="сontact-information__item--text">
+									<p>ООО "СТОМАТОЛОГИЯ ФЛАГМАН"</p>
+									<p>ОГРН: 1151690012187</p>
+								</div>
+						</div>
+					</div>
+					<div class="map">
+						
+					</div>
+			</main>
+		<?php
+			include("blocks/footer.php");
+		?>
+		    <script src="js/jquery-2.1.4.min.js"></script>
+		    <script src="js/filterservices.js"></script>
+	</body>
+</html>
